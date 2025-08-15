2f6f2f199e46cdd841e7a71df60b27eec4d64eae
sumitkshaw <shawsumit6286@gmail.com>
2025-08-15 11:57:53 +0530

    q6

---
+++ Array/easy/anagram.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+
+public class anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String t = sc.next();
+        boolean result = ana(s , t);
+        System.out.println(result);
+    }
+    public static boolean ana(String s, String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int [] count = new int[26];
+        for(int i=0; i<s.length();i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
